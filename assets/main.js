1ed2f43db283e7740f6ad8f5316312ab1d491ead
Julia White <jwhite3854@gmail.com>
2021-10-08 00:10:02 -0700

    Alchemy corrections

--- assets/main.js
+++ assets/main.js
@@ -1,104 +1,237 @@
 (function ($) {
 	'use strict';
 
-	function imageLoaded() {
-    	imagesLoaded++;
-    	if (imagesLoaded == totalImages) {
-			$grid.masonry();
-    	}
+	const conditions = {
+		"breach": "Breach",
+		"cowardice": "Cowardice",
+		"defile": "Defile",
+		"detection": "Detection",
+		"enervation": "Enervation",
+		"entrapment": "Entrapment",
+		"fracture": "Fracture",
+		"grav-health": "-- Health",
+		"heroism": "Heroism",
+		"hindrance": "Hindrance",
+		"inc-armor": "+ Armor",
+		"inc-spell-critical": "+ Spell Critical",
+		"inc-spell-power": "+ Spell Power",
+		"inc-spell-resist": "+ Spell Resist",
+		"inc-weapon-critical": "+ Weapon Critical",
+		"inc-weapon-power": "+ Weapon Power",
+		"invisible": "Invisible",
+		"ling-health": "< Health",
+		"maim": "Maim",
+		"protection": "Protection",
+		"rav-health": "- Health",
+		"rav-magicka": "- Magicka",
+		"rav-stamina": "- Stamina",
+		"res-health": "+ Health",
+		"res-magicka": "+ Magicka",
+		"res-stamina": "+ Stamina",
+		"speed": "Speed",
+		"timidity": "Timidity",
+		"uncertainty": "Uncertainty",
+		"unstoppable": "Unstoppable",
+		"vitality": "Vitality",
+		"vulnerability": "Vulnerability"
 	}
 
-	function initMasonry() {
-		let grid = $('.grid').masonry({
-			isInitLayout: false,
-			itemSelector: '.grid-item',
-			columnWidth: 120,
-			gutter: 12,
-			transitionDuration: '1.5s'
-		});
-
-		let imagesLoaded = 0;
-		let totalImages = $('img').length;
-
-		$('img').each(function(idx, img) {
-			$('<img>').on('load', imageLoaded).attr('src', $(img).attr('src'));
-		});
+	const reagents = {
+		"beetle-scuttle": {
+			"title": "Beetle Scuttle",
+			"uses": ["breach","protection","inc-armor","vitality"]
+		},
+		"blessed-thistle": {
+			"title": "Blessed Thistle",
+			"uses": ["res-stamina","inc-weapon-power","rav-health","speed"]
+		},
+		"blue-entoloma": {
+			"title": "Blue Entoloma",
+			"uses": ["rav-magicka","cowardice","res-health","invisible"]
+		},
+		"bugloss": {
+			"title": "Bugloss",
+			"uses": ["inc-spell-resist","res-health","cowardice","res-magicka"]
+		},
+		"butterfly-wing": {
+			"title": "Butterfly",
+			"uses": ["res-health","ling-health","uncertainty","vitality"]
+		},
+		"chaurus-egg": {
+			"title": "Chaurus Egg",
+			"uses": ["timidity","rav-magicka","res-stamina","detection"]
+		},
+		"clam-gall": {
+			"title": "Clam Gall",
+			"uses": ["inc-spell-resist","hindrance","vulnerability","defile"]
+		},
+		"columbine": {
+			"title": "Columbine",
+			"uses": ["res-health","res-magicka","res-stamina","unstoppable"]
+		},
+		"corn-flower": {
+			"title": "Corn Flower",
+			"uses": ["res-magicka","inc-spell-power","rav-health","detection"]
+		},
+		"dragon-rheum": {
+			"title": "Dragon Rheum",
+			"uses": ["enervation","heroism","res-magicka","speed"]
+		},
+		"dragons-bile": {
+			"title": "Dragons Bile",
+			"uses": ["heroism","vulnerability","invisible","vitality"]
+		},
+		"dragons-blood": {
+			"title": "Dragons Blood",
+			"uses": ["ling-health","res-stamina","heroism","defile"]
+		},
+		"dragonthorn": {
+			"title": "Dragonthorn",
+			"uses": ["inc-weapon-power","res-stamina","fracture","inc-weapon-critical"]
+		},
+		"emetic-russula": {
+			"title": "Emetic Russula",
+			"uses": ["rav-health","rav-magicka","rav-stamina","entrapment"]
+		},
+		"fleshfly-larva": {
+			"title": "Fleshfly Larva",
+			"uses": ["rav-stamina","grav-health","vulnerability","vitality"]
+		},
+		"imp-stool": {
+			"title": "Imp Stool",
+			"uses": ["maim","rav-stamina","inc-armor","enervation"]
+		},
+		"ladys-smock": {
+			"title": "Ladys Smock",
+			"uses": ["inc-spell-power","res-magicka","breach","inc-spell-critical"]
+		},
+		"luminous-rusula": {
+			"title": "Luminous Rusula",
+			"uses": ["rav-stamina","maim","res-health","hindrance"]
+		},
+		"mountain-flower": {
+			"title": "Mountain Flower",
+			"uses": ["inc-armor","res-health","maim","res-stamina"]
+		},
+		"mudcrab-chitin": {
+			"title": "Mudcrab Chitin",
+			"uses": ["inc-spell-resist","protection","inc-armor","defile"]
+		},
+		"namiras-rot": {
+			"title": "Namiras Rot",
+			"uses": ["inc-spell-critical","speed","invisible","unstoppable"]
+		},
+		"nightshade": {
+			"title": "Nightshade",
+			"uses": ["rav-health","grav-health","protection","defile"]
+		},
+		"nirnroot": {
+			"title": "Nirnroot",
+			"uses": ["rav-health","uncertainty","enervation","invisible"]
+		},
+		"nirnroot-crimson": {
+			"title": "Nirnroot, Crimson",
+			"uses": ["timidity","inc-spell-critical","grav-health","res-health"]
+		},
+		"pearl": {
+			"title": "Powdered Mother of Pearl",
+			"uses": ["ling-health","speed","vitality","protection"]
+		},
+		"scrib-jelly": {
+			"title": "Scrib Jelly",
+			"uses": ["rav-magicka","vulnerability","speed","ling-health"]
+		},
+		"spider-egg": {
+			"title": "Spider Egg",
+			"uses": ["hindrance","ling-health","invisible","defile"]
+		},
+		"stinkhorn": {
+			"title": "Stinkhorn",
+			"uses": ["fracture","rav-health","inc-weapon-power","rav-stamina"]
+		},
+		"torchbug-thorax": {
+			"title": "Torchbug Thorax",
+			"uses": ["fracture","enervation","detection","vitality"]
+		},
+		"vile-coagulant": {
+			"title": "Vile Coagulant",
+			"uses": ["timidity","rav-health","res-magicka","protection"]
+		},
+		"violet-coprinus": {
+			"title": "Violet Coprinus",
+			"uses": ["breach","rav-health","inc-spell-power","rav-magicka"]
+		},
+		"water-hyacinth": {
+			"title": "Water Hyacinth",
+			"uses": ["res-health","inc-spell-critical","inc-weapon-critical","entrapment"]
+		},
+		"white-cap": {
+			"title": "White Cap",
+			"uses": ["cowardice","rav-magicka","inc-spell-resist","detection"]
+		},
+		"wormwood": {
+			"title": "Wormwood",
+			"uses": ["inc-weapon-critical","hindrance","detection","unstoppable"]
+		}
 	}
 
-	function toggleFavorite(element) {
-		let src = $(element).data('src');
-		$.post( "api/toggle-favorites", { src: src }).done(function( data ) {
-			if (data) {
-				$(element).removeClass('chosen');
-				if ($(element).parent(".mb-3.favorites")) {
-					$(element).parent(".mb-3.favorites").remove();
-				}
-			} else {
-				$(element).addClass('chosen');
-			}
-		});
-	}
+	const reagentList = document.getElementById("reagent-list");
+	for (const [id, reagent] of Object.entries(reagents)){
 
-	function toggleSelectMode(element) {
-		let enabled = $(element).hasClass('btn-info');
+		let liDataTags = id + ' ' + reagent.uses.join(' ');
 
-		if (enabled) {
-			$(element).addClass('btn-secondary').removeClass('btn-info').html("Select Mode: Off");
-			$(".favToggler").addClass("d-none");
-		} else {
-			$(element).addClass('btn-info').removeClass('btn-secondary').html("Select Mode: On");
-			$(".favToggler").removeClass("d-none");
+		let cardInput = document.createElement("input");
+		cardInput.className = "reagent";
+		cardInput.setAttribute("type", "checkbox");
+		cardInput.setAttribute("id", "reagent-"+id);
+		cardInput.setAttribute("value", id);
+
+		let cardTitle = document.createElement("label");
+		cardTitle.className = "uk-card-title";
+		cardTitle.innerText = reagent.title;
+		cardTitle.setAttribute("for", "reagent-"+id);
+		
+		let cardHeader = document.createElement("div");
+		cardHeader.className = "uk-card-header";
+		cardHeader.append(cardInput);
+		cardHeader.append(cardTitle);
+
+		let cardGrid = document.createElement("div");
+		cardGrid.className = "reagent-filter uk-grid uk-child-width-1-2 uk-child-width-1-4@m uk-text-center";
+		cardGrid.setAttribute("uk-grid", "uk-grid");
+
+		for (let useID of reagent.uses) {
+			let reagentButton = document.createElement("button");
+			reagentButton.className = "uk-button uk-button-default uk-width-1-1";
+			reagentButton.innerText = conditions[useID];
+
+			let reagentDiv = document.createElement("div");
+			reagentDiv.setAttribute("uk-filter-control", "[data-tags*='"+useID+"']");
+			reagentDiv.setAttribute("data-tags", id + " " + useID);
+			reagentDiv.append(reagentButton);
+
+			cardGrid.append(reagentDiv);
 		}
 
-		$.post( "api/toggle-favorite-mode", { enabled: enabled });
+		let cardBody = document.createElement("div");
+		cardBody.className = "uk-card-body";
+		cardBody.append(cardGrid);
+
+		let listEl = document.createElement("li");
+		listEl.setAttribute('data-tags', liDataTags);
+		listEl.append(cardHeader);
+		listEl.append(cardBody);
+		
+		reagentList.append(listEl);
 	}
 
-	function toggleViewMode(element) {
-		let grid = $(element).hasClass('viewGrid');
-
-		if (grid) {
-			$(element).removeClass('viewGrid').html("View Mode: Column");
-		} else {
-			$(element).addClass('viewGrid').html("View Mode: Masonry");
-			$(".favorites").addClass("grid");
-			let grid = $('.grid');
-			if ( grid.length > 0 ) {
-				initMasonry();
-			}
-		}
-	}
-
-	function clearAllFavorites() {
-		$.post( "api/clearall-favorites", { src: 1 }).done(function() {
-			location.reload();
+	const reagentInputs = document.getElementsByClassName('reagent');
+	for (const reagent of reagentInputs){
+		reagent.addEventListener("click", function(e){
+			let reagentUses = reagents[reagent.value].uses;
+			console.log(reagent.checked);
+			
 		});
 	}
 
-	$(document).ready(function() {
-		let items = $('.item');
-		if ( items.length > 0 ) {
-			items.simpleLightbox();
-		}
-
-		let grid = $('.grid');
-		if ( grid.length > 0 ) {
-			initMasonry();
-		}
-
-		$('.container').on("click", '.favToggler', function(e){
-			toggleFavorite(this);
-		});
-
-		$('.container').on("click", '#enableSelect', function(e){
-			toggleSelectMode(this);
-		});
-
-		$('.container').on("click", '#toggleView', function(e){
-			toggleViewMode(this);
-		});
-
-		$('.container').on("click", '#clearAll', function(e){
-			clearAllFavorites();
-		});
-	});
-})(jQuery);
+})();
